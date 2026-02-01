3ae6621781b5d0a2b53375be5d0fb30d6815f37a
Roman Dietenmeier <roman@d-robotic.de>
2026-02-01 11:33:53 +0100

    Create a Sudoku field

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,119 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>Hello World</>
+    <>
+      <table
+        style={{
+          borderCollapse: "collapse",
+          aspectRatio: "1 / 1",
+          width: "100%",
+        }}
+      >
+        <tr>
+          <td className="table_cell table_cell_border_top table_cell_border_left"></td>
+          <td className="table_cell table_cell_border_top">9</td>
+          <td className="table_cell table_cell_border_top table_cell_border_right">
+            1
+          </td>
+          <td className="table_cell table_cell_border_top"></td>
+          <td className="table_cell table_cell_border_top"></td>
+          <td className="table_cell table_cell_border_top table_cell_border_right"></td>
+          <td className="table_cell table_cell_border_top"></td>
+          <td className="table_cell table_cell_border_top"></td>
+          <td className="table_cell table_cell_border_top table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell "></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_bottom"></td>
+          <td className="table_cell table_cell_border_right table_cell_border_bottom"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_left"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell"></td>
+          <td className="table_cell table_cell_border_right"></td>
+        </tr>
+        <tr>
+          <td className="table_cell table_cell_border_bottom  table_cell_border_left"></td>
+          <td className="table_cell table_cell_border_bottom "></td>
+          <td className="table_cell table_cell_border_bottom table_cell_border_right"></td>
+          <td className="table_cell table_cell_border_bottom "></td>
+          <td className="table_cell table_cell_border_bottom "></td>
+          <td className="table_cell table_cell_border_bottom table_cell_border_right"></td>
+          <td className="table_cell table_cell_border_bottom "></td>
+          <td className="table_cell table_cell_border_bottom "></td>
+          <td className="table_cell table_cell_border_bottom  table_cell_border_right"></td>
+        </tr>
+      </table>
+    </>
   </StrictMode>,
 );
