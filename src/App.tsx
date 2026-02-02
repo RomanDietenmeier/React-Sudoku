cec68d1decc28f03455f5c5032736350effd789d
Roman Dietenmeier <roman@d-robotic.de>
2026-02-02 18:19:36 +0100

    Add some text what should happen to this repository, Future Work to make this a good Sudoku thing. Also add references from Lukas Ricahrd on how to add more solving strategies.

--- src/App.tsx
+++ src/App.tsx
@@ -598,6 +598,52 @@ export function App() {
         </tbody>
       </table>
       <button onClick={onClickSolveStep}>Solve Step</button>
+      <div>
+        <h2>ToDo / Further Steps</h2>
+        <p>
+          The "Solve Step" solver is not able to solve Sudokus with the 2
+          candidates problem or harder.
+        </p>
+        <p>
+          I guess I could add a brute-force solver, but that is not what I like.
+        </p>
+        <p>This is such a Sudoku:</p>
+        <p className="font_monospace">
+          <div>[</div>
+          <div>-1, -1, -1, &nbsp;6, -1, &nbsp;4, -1, -1, -1, </div>
+          <div>-1, -1, -1, -1, &nbsp;8, -1, -1, -1, &nbsp;6, </div>
+          <div>-1, -1, &nbsp;6, &nbsp;9, -1, -1, -1, &nbsp;3, &nbsp;8,</div>
+          <div>&nbsp;2, -1, -1, -1, -1, &nbsp;6, &nbsp;5, -1, -1,</div>
+          <div>&nbsp;5, &nbsp;7, -1, -1, -1, -1, -1, &nbsp;6, &nbsp;1,</div>
+          <div>-1, -1, &nbsp;3, &nbsp;1, -1, -1, -1, -1, &nbsp;4,</div>
+          <div>&nbsp;9, &nbsp;6, -1, -1, -1, &nbsp;7, &nbsp;2, -1, -1,</div>
+          <div>&nbsp;7, -1, -1, -1, &nbsp;1, -1, -1, -1, -1, </div>
+          <div>-1, -1, -1, &nbsp;8, -1, &nbsp;9, -1, -1, -1, </div>
+          <div>]</div>
+        </p>
+        <p>
+          Nonetheless, this is a working Sudoku-Board! With an active cell and
+          error highlighting.
+        </p>
+        <p>ENJOY</p>
+        <p>P.S. I guess I should also add a Sudoku Creator ^^</p>
+        <p>
+          I think I should read the book from Lukas Richard on Sudoku
+          Strategies:{" "}
+          <center>
+            <a href="https://lr-raetsel.de/sudoku-strategien-buch/">
+              Sudoku Strategien Buch - Lukas Richard
+            </a>
+          </center>
+          <br />
+          He also has an excelent Website on the Strategies:
+          <center>
+            <a href="https://sudoku-hilfe.de/direkter_zweifacher_kandidat">
+              sudoku-hilfe.de/direkter_zweifacher_kandidat
+            </a>
+          </center>
+        </p>
+      </div>
     </>
   );
 }
