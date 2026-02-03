13fc360cc8e27d03d7708ca0f9cb00ecb44ce9ed
Roman Dietenmeier <roman@d-robotic.de>
2026-02-03 22:19:05 +0100

    Remove the further work text as I want to embed this on my website.

--- src/App.tsx
+++ src/App.tsx
@@ -753,65 +753,6 @@ export function App() {
           </button>
         </div>
       </div>
-      <div>
-        <h2>ToDo / Further Steps</h2>
-        <p>
-          The "Solve Step" solver is not able to solve Sudokus with the 2
-          candidates problem or harder.
-        </p>
-        <p>
-          I guess I could add a brute-force solver, but that is not what I like.
-        </p>
-        <p>This is such a Sudoku:</p>
-        <div className="font_monospace">
-          <div>[</div>
-          <div>-1, -1, -1, &nbsp;6, -1, &nbsp;4, -1, -1, -1, </div>
-          <div>-1, -1, -1, -1, &nbsp;8, -1, -1, -1, &nbsp;6, </div>
-          <div>-1, -1, &nbsp;6, &nbsp;9, -1, -1, -1, &nbsp;3, &nbsp;8,</div>
-          <div>&nbsp;2, -1, -1, -1, -1, &nbsp;6, &nbsp;5, -1, -1,</div>
-          <div>&nbsp;5, &nbsp;7, -1, -1, -1, -1, -1, &nbsp;6, &nbsp;1,</div>
-          <div>-1, -1, &nbsp;3, &nbsp;1, -1, -1, -1, -1, &nbsp;4,</div>
-          <div>&nbsp;9, &nbsp;6, -1, -1, -1, &nbsp;7, &nbsp;2, -1, -1,</div>
-          <div>&nbsp;7, -1, -1, -1, &nbsp;1, -1, -1, -1, -1, </div>
-          <div>-1, -1, -1, &nbsp;8, -1, &nbsp;9, -1, -1, -1, </div>
-          <div>]</div>
-        </div>
-        <p>
-          Nonetheless, this is a working Sudoku-Board! With an active cell and
-          error highlighting.
-        </p>
-        <p>ENJOY</p>
-        <p>P.S. I guess I should also add a Sudoku Creator ^^</p>
-        <p>
-          I think I should read the book from Lukas Richard on Sudoku
-          Strategies: <br />
-          <span
-            style={{
-              display: "inline-block",
-              textAlign: "center",
-              width: "100%",
-            }}
-          >
-            <a href="https://lr-raetsel.de/sudoku-strategien-buch/">
-              Sudoku Strategien Buch - Lukas Richard
-            </a>
-          </span>
-          <br />
-          He also has an excelent Website on the Strategies:
-          <br />
-          <span
-            style={{
-              display: "inline-block",
-              textAlign: "center",
-              width: "100%",
-            }}
-          >
-            <a href="https://sudoku-hilfe.de/direkter_zweifacher_kandidat">
-              Sudoku-Hilfe.de/direkter_zweifacher_kandidat
-            </a>
-          </span>
-        </p>
-      </div>
     </>
   );
 }
